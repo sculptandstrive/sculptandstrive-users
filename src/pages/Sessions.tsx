079b3d7655d87244a3f0e1476ddc3dac3d554d95
hunterckm01 <huner6894@gmail.com>
2026-02-19 11:18:14 +0530

    Fixed Recorded Session

--- src/pages/Sessions.tsx
+++ src/pages/Sessions.tsx
@@ -126,7 +126,10 @@ export default function Sessions() {
     .map((s) => {
       const now = new Date();
       const scheduled = new Date(s.scheduled_at);
-      const endTime = new Date(scheduled.getTime() + 45 * 60 * 1000);
+      let endTime = new Date(scheduled.getTime() + 45 * 60 * 1000);
+      if(s.type === "recorded"){
+        endTime = new Date(scheduled.getTime() + 1000 * 60 * 60 * 24 * 7);
+      }
 
       const isSameDay =
         now.getFullYear() === scheduled.getFullYear() &&
